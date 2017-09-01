c04e1b998a2d837d356621dc4f5d255eb0e97ca6
Alberto97 <albertop2197@gmail.com>
2017-09-01 13:04:24 +0200

    msm8916-common: Add camera shim
    Change-Id: I616c7b65abab99fbf5adfd1824c18c4a4a2f9019

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -21,3 +21,14 @@ LOCAL_SHARED_LIBRARIES := libgui
 LOCAL_MODULE := libshim_ril
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := MediaCodec.cpp
+
+LOCAL_SHARED_LIBRARIES := libstagefright libmedia
+
+LOCAL_MODULE := libshims_camera
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
